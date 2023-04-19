3150a2ad00eb253aaabcd38a0e670f7be1be1b89
arij33tt <arijeetaman@gmail.com>
2023-04-20 01:42:54 +0530

    MySql-React Book App

---
+++ src/pages/Add.jsx
@@ -0,0 +1,46 @@
+import axios from 'axios'
+import React, { useState } from 'react'
+import { useNavigate } from 'react-router-dom'
+
+const Add = () => {
+    const[book,setBook]= useState({
+        title:"",
+        desc:"",
+        price:null,
+        cover:""
+    })
+
+    const navigate =useNavigate()
+
+    const handleChange=(e)=>{
+        setBook((prev)=>({...prev , [e.target.name]: e.target.value}))
+    }
+
+    const handleClick= async e=>{
+        e.preventDefault();
+
+        try{
+            await axios.post("http://localhost:6999/books",book)
+            navigate("/")
+        }
+        catch(err){
+            console.log(err);
+        }
+
+    }
+    // console.log(book);
+  return (
+    <div className='form'><h1>
+        Add New Book
+    </h1>
+
+        <input type="text" placeholder='title'  onChange={handleChange} name='title'/>
+        <input type="text" placeholder='description' onChange={handleChange}  name='desc'/>
+        <input type="number" placeholder='price'  onChange={handleChange} name='price'/>
+        <input type="text" placeholder='cover' onChange={handleChange}  name='cover'/>
+        <button onClick={handleClick} className='formButton'>Add</button>
+    </div>
+  )
+}
+
+export default Add
